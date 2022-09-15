33ea382c5ce1aee4ac79efb81b7d642d94890882
李翔 <1058684043@qq.com>
2022-09-15 10:48:39 +0800

    fix: close test

--- packages/provider/tests/keyset.spec.ts
+++ packages/provider/tests/keyset.spec.ts
@@ -1,8 +1,8 @@
-import UnipassWalletProvider from "../src/index";
+// import UnipassWalletProvider from "../src/index";
 
 describe("Test Keyset", () => {
   it("Create Keyset Should Success", async () => {
-    const wallet = new UnipassWalletProvider({ chainName: "polygon", env: "dev" });
-    console.log(wallet);
+    // const wallet = new UnipassWalletProvider({ chainName: "polygon", env: "dev" });
+    // console.log(wallet);
   });
 });
