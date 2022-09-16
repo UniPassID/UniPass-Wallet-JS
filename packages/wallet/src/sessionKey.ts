1f49278b9dec83f95fc95a880703e88748b5de9b
zz <517669936@qq.com>
2022-09-16 21:17:50 +0800

    feat: add to SessionKeyStore for SessionKey

--- packages/wallet/src/sessionKey.ts
+++ packages/wallet/src/sessionKey.ts
@@ -1,8 +1,19 @@
 import { BytesLike, utils, Wallet as WalletEOA } from "ethers";
-import { sign, SignType } from "@unipasswallet/keys";
+import { KeySecp256k1, sign, SignType } from "@unipasswallet/keys";
 import { IPermit } from "./permit";
 import { subDigest } from "@unipasswallet/utils";
 import { Wallet } from "./wallet";
+
+export interface SessionKeyStore {
+  localKey: {
+    keystore: string;
+    address: string;
+  };
+  aesKey: CryptoKey;
+  authorization: string;
+  expires: number;
+  weight: number;
+}
 
 export class SessionKey {
   public readonly userAddr: string;
@@ -53,5 +64,34 @@ export class SessionKey {
         this.permit!.permit,
       ],
     );
+  }
+
+  static async fromSessionKeyStore(
+    store: SessionKeyStore,
+    _wallet: Wallet,
+    aesDecryptor: (aesKey: CryptoKey, sig: BytesLike) => Promise<string>,
+  ): Promise<SessionKey> {
+    const { keyset } = _wallet;
+    const privateKey = await aesDecryptor(store.aesKey, store.localKey.keystore);
+
+    const materKey = keyset.keys[0];
+    if (KeySecp256k1.isKeySecp256k1(materKey)) {
+      keyset.keys[0] = new KeySecp256k1(
+        materKey.address,
+        materKey.roleWeight,
+        materKey.getSignType(),
+        async () => store.authorization,
+      );
+    } else {
+      throw new Error("Master Key Should By KeySecp256k1");
+    }
+
+    const wallet = _wallet.setKeyset(keyset);
+
+    let sessionKey = new SessionKey(new WalletEOA(privateKey), SignType.EthSign, store.localKey.address);
+
+    sessionKey = await sessionKey.generatePermit(store.expires, store.weight, wallet, [0]);
+
+    return sessionKey;
   }
 }
